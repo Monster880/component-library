2ef5f8ac2865dfb0513eb185d1e4e0a921d2a8ed
Li Xiao <539561425@qq.com>
2020-10-07 20:43:04 +0800

    feat: button组件创建

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import Button, { ButtonType, ButtonSize } from './components/Button/button';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Button disabled={true}> Hello  </Button>
+        <Button btnType= {ButtonType.Primary} size={ButtonSize.Large} autoFocus> Hello  </Button>
+        <Button btnType= {ButtonType.Danger} size={ButtonSize.Small}> Hello  </Button>
+        <Button btnType= {ButtonType.Default} size={ButtonSize.Large}> Hello  </Button>
+        <Button btnType= {ButtonType.Default} size={ButtonSize.Small}> Hello  </Button>
+        <Button btnType= {ButtonType.Link} href="https://www.baidu.com" disabled={true}> 百度Link  </Button>
+        <Button btnType= {ButtonType.Link} href="https://www.baidu.com" target="_blank"> 百度Link  </Button>
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
